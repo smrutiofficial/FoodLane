117a9244f4665f1591bdb0badf246c578c3a3729
Smruti Prakash Rout <smrutiprakashrout3@gmail.com>
2024-01-08 11:57:50 +0530

    yt

---
+++ api/card.js
@@ -0,0 +1,161 @@
+export const card = [
+    {
+        id: 1,
+        img: "./src/dish/1.jpg",
+        title: "Dal Makhani",
+        tagline: "Rich lentils, tomato gravy, cream delight.",
+        des: "Rich Black lentil delicacy, prepared with lushes tomato gravy, cream and butter served with choice of breads or rice served with choice of breads or rice.",
+        price: 219,
+        featured: false,
+        rate: "3.9(54)"
+    },
+    {
+        id: 2,
+        img: "./src/dish/2.png",
+        title: "Spicy Mix Vegetable",
+        tagline: "Seasonal veggies in rich Indian spices.",
+        des: "Seasonal vegetables tossed in rich Indian spices",
+        price: 149,
+        featured: false,
+        rate: "4.4(88)"
+    },
+    {
+        id: 3,
+        img: "./src/dish/5.jpg",
+        title: "Masala Soya Chaap",
+        tagline: "Soya chaap, aromatic spices, tomato gravy.",
+        des: "Soya chaap cooked with aromatic spices, onion & tomato gravy",
+        price: 279,
+        featured: false,
+        rate: "4.7(30)"
+    },
+    {
+        id: 4,
+        img: "./src/dish/4.jpg",
+        title: "Paneer Butter Masala",
+        tagline: "Spicy tomato Makhani, Cottage cheese delight.",
+        des: "Rich and spicy tomato Makhani Gravy with Cottage cheese finished with Butter",
+        price: 279,
+        featured: false,
+        rate: "4.3(43)"
+    },
+    {
+        id: 5,
+        img: "./src/dish/6.jpg",
+        title: "Pao Bhaji",
+        tagline: "Mumbai Style Spicy Mashed Vegetables Delight.",
+        des: "Mumbai Style Spicy Mashed Vegetables Served With Buttery Bread Roll",
+        price: 199,
+        featured: false,
+        rate: "4.3(75)"
+    },
+    {
+        id: 6,
+        img: "./src/dish/7.jpg",
+        title: "Butter Chicken",
+        tagline: "Smooth tomato Makhani, tandoori chicken delight.",
+        des: "Smooth and rich tomato-based makhani gravy with cream and butter with tandoori chicken",
+        price: 349,
+        featured: true,
+        rate: "4.0(88)"
+    },
+    {
+        id: 7,
+        img: "./src/dish/8.jpg",
+        title: "Special Veg Thali",
+        tagline: "Dal Makhani, Paneer, Mix Veg, Indian feast.",
+        des: "(Dal Makhani+ Paneer Of The Day+ Mix Veg + Raita + Rice+ 1 Butter Naan + 1 Butter Roti+ Gulab Jamun+ Salad)",
+        price: 419,
+        featured: false,
+        rate: "3.9(165)"
+    },
+    {
+        id: 8,
+        img: "./src/dish/9.jpg",
+        title: "Tangri Kebab",
+        tagline: "Tender chicken drumstick, tandoori marination.",
+        des: "Tender and succulent chicken drumstick, tandoori masala marination",
+        price: 399,
+        featured: false,
+        rate: "4.3(131)"
+    },
+    {
+        id: 9,
+        img: "./src/dish/10.jpg",
+        title: "Punjabi Chole",
+        tagline: "Classic Punjab dish, flavorful spice blend.",
+        des: "A classic dish from the Punjab, which is flavored with a large assortment of spice powders",
+        price: 219,
+        featured: true,
+        rate: "3.8(58)"
+    },
+    {
+        id: 10,
+        img: "./src/dish/11.jpeg",
+        title: "Chicken Seekh Kebab",
+        tagline: "Chicken on bone, malai marination delight.",
+        des: "Chicken on bone, malai marination, cooked in tandoor",
+        price: 369,
+        featured: false,
+        rate: "4.8(64)"
+    },
+    {
+        id: 11,
+        img: "./src/dish/12.jpg",
+        title: "Dum Chicken Biryani",
+        tagline: "Slow-cooked, flavorsome Chicken, Basmati Rice.",
+        des: "Traditional slow-cooked, flavorsome Chicken lag with Long Grain Basmati Rice Served with Raita",
+        price: 379,
+        featured: false,
+        rate: "4.1(170)"
+    },
+    {
+        id: 12,
+        img: "./src/dish/14.jpeg",
+        title: "Butter Chicken Roll",
+        tagline: "Authentic butter chicken in ulta tawa.",
+        des: "Authentic butter chicken wrapped in a ulta tawa",
+        price: 249,
+        featured: false,
+        rate: "4.0(26)"
+    },
+    {
+        id: 13,
+        img: "./src/dish/15.jpeg",
+        title: "Kadhai Paneer Gravy",
+        tagline: "Perfect blend, onion tomato tadka, spices.",
+        des: "Prefect blend of onion tomato tadka and indian spices with Cottage Cheese and bell peppers",
+        price: 279,
+        featured: false,
+        rate: "4.4(64)"
+    },
+    {
+        id: 14,
+        img: "./src/dish/18.jpg",
+        title: "Chilly Chicken Dry",
+        tagline: "Boneless chicken, red chilies.",
+        des: "Made of chunks of boneless chicken mixed with dry red chilies, finely chopped ginger and garlic, onions and other Indo Chinese spices to make a spicy Indian flare dish along with the distinct traditional Chinese flavors",
+        price: 220,
+        featured: true,
+        rate: "3.6(745)"
+    },
+    {
+        id: 15,
+        img: "./src/dish/19.jpg",
+        title: "Dragon Chicken",
+        tagline: "Batter-fried, spices, Indo Chinese delight.",
+        des: "Dragon chicken is a popular Indo Chinese non-vegetarian starter recipe. Dragon chicken is made from batter-fried chicken and then cooked in lots of spices.",
+        price: 214,
+        featured: false,
+        rate: "4.1(256)"
+    },
+    {
+        id: 16,
+        img: "./src/dish/20.jpeg",
+        title: "Mexican Pizza",
+        tagline: "Flavorful mix mozzarella delight.",
+        des: "Serves 1 | Flavourful mix of red capsicum, green capsicum, jalapeno, onion, black olives, sweet corn and 100% mozzarella Cheese with a signature spice sprinkle & our flavourful pan sauce.",
+        price: 319,
+        featured: true
+    }
+];
--- main.js
+++ main.js
@@ -1,164 +1,6 @@
-const card = [
-    {
-        id: 1,
-        img: "./src/dish/1.jpg",
-        title: "Dal Makhani",
-        tagline: "Rich lentils, tomato gravy, cream delight.",
-        des: "Rich Black lentil delicacy, prepared with lushes tomato gravy, cream and butter served with choice of breads or rice served with choice of breads or rice.",
-        price: 219,
-        featured: false,
-        rate: "3.9(54)"
-    },
-    {
-        id: 2,
-        img: "./src/dish/2.png",
-        title: "Spicy Mix Vegetable",
-        tagline: "Seasonal veggies in rich Indian spices.",
-        des: "Seasonal vegetables tossed in rich Indian spices",
-        price: 149,
-        featured: false,
-        rate: "4.4(88)"
-    },
-    {
-        id: 3,
-        img: "./src/dish/5.jpg",
-        title: "Masala Soya Chaap",
-        tagline: "Soya chaap, aromatic spices, tomato gravy.",
-        des: "Soya chaap cooked with aromatic spices, onion & tomato gravy",
-        price: 279,
-        featured: false,
-        rate: "4.7(30)"
-    },
-    {
-        id: 4,
-        img: "./src/dish/4.jpg",
-        title: "Paneer Butter Masala",
-        tagline: "Spicy tomato Makhani, Cottage cheese delight.",
-        des: "Rich and spicy tomato Makhani Gravy with Cottage cheese finished with Butter",
-        price: 279,
-        featured: false,
-        rate: "4.3(43)"
-    },
-    {
-        id: 5,
-        img: "./src/dish/6.jpg",
-        title: "Pao Bhaji",
-        tagline: "Mumbai Style Spicy Mashed Vegetables Delight.",
-        des: "Mumbai Style Spicy Mashed Vegetables Served With Buttery Bread Roll",
-        price: 199,
-        featured: false,
-        rate: "4.3(75)"
-    },
-    {
-        id: 6,
-        img: "./src/dish/7.jpg",
-        title: "Butter Chicken",
-        tagline: "Smooth tomato Makhani, tandoori chicken delight.",
-        des: "Smooth and rich tomato-based makhani gravy with cream and butter with tandoori chicken",
-        price: 349,
-        featured: true,
-        rate: "4.0(88)"
-    },
-    {
-        id: 7,
-        img: "./src/dish/8.jpg",
-        title: "Special Veg Thali",
-        tagline: "Dal Makhani, Paneer, Mix Veg, Indian feast.",
-        des: "(Dal Makhani+ Paneer Of The Day+ Mix Veg + Raita + Rice+ 1 Butter Naan + 1 Butter Roti+ Gulab Jamun+ Salad)",
-        price: 419,
-        featured: false,
-        rate: "3.9(165)"
-    },
-    {
-        id: 8,
-        img: "./src/dish/9.jpg",
-        title: "Tangri Kebab",
-        tagline: "Tender chicken drumstick, tandoori marination.",
-        des: "Tender and succulent chicken drumstick, tandoori masala marination",
-        price: 399,
-        featured: false,
-        rate: "4.3(131)"
-    },
-    {
-        id: 9,
-        img: "./src/dish/10.jpg",
-        title: "Punjabi Chole",
-        tagline: "Classic Punjab dish, flavorful spice blend.",
-        des: "A classic dish from the Punjab, which is flavored with a large assortment of spice powders",
-        price: 219,
-        featured: true,
-        rate: "3.8(58)"
-    },
-    {
-        id: 10,
-        img: "./src/dish/11.jpeg",
-        title: "Chicken Seekh Kebab",
-        tagline: "Chicken on bone, malai marination delight.",
-        des: "Chicken on bone, malai marination, cooked in tandoor",
-        price: 369,
-        featured: false,
-        rate: "4.8(64)"
-    },
-    {
-        id: 11,
-        img: "./src/dish/12.jpg",
-        title: "Dum Chicken Biryani",
-        tagline: "Slow-cooked, flavorsome Chicken, Basmati Rice.",
-        des: "Traditional slow-cooked, flavorsome Chicken lag with Long Grain Basmati Rice Served with Raita",
-        price: 379,
-        featured: false,
-        rate: "4.1(170)"
-    },
-    {
-        id: 12,
-        img: "./src/dish/14.jpeg",
-        title: "Butter Chicken Roll",
-        tagline: "Authentic butter chicken in ulta tawa.",
-        des: "Authentic butter chicken wrapped in a ulta tawa",
-        price: 249,
-        featured: false,
-        rate: "4.0(26)"
-    },
-    {
-        id: 13,
-        img: "./src/dish/15.jpeg",
-        title: "Kadhai Paneer Gravy",
-        tagline: "Perfect blend, onion tomato tadka, spices.",
-        des: "Prefect blend of onion tomato tadka and indian spices with Cottage Cheese and bell peppers",
-        price: 279,
-        featured: false,
-        rate: "4.4(64)"
-    },
-    {
-        id: 14,
-        img: "./src/dish/18.jpg",
-        title: "Chilly Chicken Dry",
-        tagline: "Boneless chicken, red chilies.",
-        des: "Made of chunks of boneless chicken mixed with dry red chilies, finely chopped ginger and garlic, onions and other Indo Chinese spices to make a spicy Indian flare dish along with the distinct traditional Chinese flavors",
-        price: 220,
-        featured: true,
-        rate: "3.6(745)"
-    },
-    {
-        id: 15,
-        img: "./src/dish/19.jpg",
-        title: "Dragon Chicken",
-        tagline: "Batter-fried, spices, Indo Chinese delight.",
-        des: "Dragon chicken is a popular Indo Chinese non-vegetarian starter recipe. Dragon chicken is made from batter-fried chicken and then cooked in lots of spices.",
-        price: 214,
-        featured: false,
-        rate: "4.1(256)"
-    },
-    {
-        id: 16,
-        img: "./src/dish/20.jpeg",
-        title: "Mexican Pizza",
-        tagline: "Flavorful mix mozzarella delight.",
-        des: "Serves 1 | Flavourful mix of red capsicum, green capsicum, jalapeno, onion, black olives, sweet corn and 100% mozzarella Cheese with a signature spice sprinkle & our flavourful pan sauce.",
-        price: 319,
-        featured: true
-    }
-]
+import * as carditem from "./api/card.js";
+const card = carditem.card;
+
 // ---------------------------------------------------------------------------
 const cardct = document.querySelector(".m2")
 const display = card.map((item, index) => {
@@ -178,7 +20,7 @@ cardct.innerHTML = display.join("");
 
 const fav = document.querySelector(".swiper-wrapper");
 const dsfav = card.map((item, index) => {
-    if (item.featured==true) {
+    if (item.featured == true) {
         return `
         <div class="swiper-slide">
         <!--  -->
@@ -209,7 +51,7 @@ const dsfav = card.map((item, index) => {
         </div>
         <!--  -->
     </div>
-        ` 
+        `
     }
 
 })
@@ -264,6 +106,7 @@ var swiper = new Swiper(".mySwiper", {
     mousewheel: true,
     keyboard: true,
 });
+
 // review
 let usertexts = document.getElementsByClassName("us_text");
 let userpics = document.getElementsByClassName("user_pic");
@@ -280,12 +123,13 @@ function showreview() {
     usertexts[i].classList.add("activetext");
 }
 
-
-
 // cart
 let snav = document.querySelector(".nav");
 let cartwraper = document.querySelector(".cartsection");
-
+let cartcov =document.querySelector(".cart");
+cartcov.onclick = function() {
+    ac_cart()
+};
 function ac_cart() {
     if (cartwraper.style.display === "none") {
         cartwraper.style.display = "flex";
@@ -305,7 +149,6 @@ const cartButtons = document.querySelectorAll(".btn5");
 let list = [];
 const cart_data = document.querySelector(".cart_wraper");
 const totalpricese = document.querySelector(".totalprice");
-
 
 function addtocart(key) {
     // console.log(id);
@@ -328,6 +171,7 @@ function addtocart(key) {
         alert("Item already in cart")
     }
 }
+window.addtocart = addtocart;
 
 
 
